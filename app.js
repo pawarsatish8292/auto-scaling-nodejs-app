40f6f589079dd7d879bb0636288b3b9355923a9c
pawarsatish8292@gmail.com <satish.pawar@thefitway.io>
2023-08-26 23:11:50 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 3001;
+const port = 3000;
 app.get('/get',(req,res)=>{
     res.send('<h1>Auto Scaling Demo App </h1> <h4>Message: Success</h4> <p> Version: 1.0.0 </P>');
 })
